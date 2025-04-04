da76356ed35443b6542b445ff8fd9fb2f499559a
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-04-04 19:25:15 +0530

    sum

--- recursion.js
+++ recursion.js
@@ -13,18 +13,24 @@
 // }
 // console.log(fibonacci(6))
 
-function binary(arr,target){
-    let left = 0;
-    let right = arr.length-1;
-    while(left<=right){
-        let mid = Math.floor((left+right) /2);
-        if(arr[mid]===target){
-            return mid;
-        }else if(arr[mid]<target){
-            left = mid+1
-        }else rigth = mid-1
-    }
-    return -1
+// function binary(arr,val){
+//     let left = 0;
+//     let right = arr.length-1;
+//     while(left<=right){
+//         let mid = Math.floor((left+right)/2);
+//         if(arr[mid]===val){
+//             return mid
+//         }else if(arr[mid]<val){
+//             left = mid+1
+//         }else right = mid-1
+//     }return -1
+// }
+// let arr = [1,2,3,4,5]
+// console.log(binary(arr,3))
+
+function sum (arr){
+    if(arr.length ==0)return 0;
+    return arr[0]+sum(arr.slice(1))
 }
-let arr = [1,2,3,4,5,6,7,8,9]
-console.log(binary(arr,9))
+let arr = [1,2,3,4,5]
+console.log(sum(arr))
