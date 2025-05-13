b940c065324433b590ffe866088b3fdb66a3ea9a
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-05-13 10:28:26 +0530

    Updated login page design

---
+++ binarytreemain.js
@@ -0,0 +1,49 @@
+class BinaryNode{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Binarytree{
+    constructor(){
+        this.root = null;
+    }
+
+    isEmpty(){
+        return this.root === null;
+    }
+
+    insert(value){
+        let node = new BinaryNode(value)
+        if(this.isEmpty()){
+            this.root = node;
+            return;
+        }
+        let queue = [this.root];
+        while(queue.length){
+            let curr = queue.shift()
+            if(!curr.left){
+                curr.left = node;
+                return
+            }else {
+                queue.push(curr.left)
+            }
+            if(!curr.right){
+                curr.right = node;
+                return 
+            }else{
+                queue.push(curr.right)
+            }
+        }
+    }
+}
+
+const bt = new Binarytree()
+
+bt.insert(20)
+bt.insert(45)
+bt.insert(15)
+console.log(bt.isEmpty())
+console.log(JSON.stringify(bt.root, null, 2));
