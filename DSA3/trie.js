ab38c56007492c05ff556b488315d2cca6aa8ce3
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-06-24 12:41:14 +0530

    trie implemented succesfully

--- DSA3/trie.js
+++ DSA3/trie.js
@@ -9,68 +9,90 @@ class Trie{
         this.root = new Node()
     }
     insert(word){
-        let current = this.root
+        let curr = this.root
         for(let char of word){
-            if(!current.children[char]){
-                current.children[char] = new Node()
-            }current = current.children[char]
-        }current.isend = true
+            if(!curr.children[char]){
+                curr.children[char] = new Node()
+            }curr = curr.children[char]
+        }curr.isend = true
     }
     search(word){
-        let current = this.root
+        let curr = this.root
         for(let char of word){
-            if(!current.children[char]){
+            if(!curr.children[char]){
                 return false
-            }current = current.children[char]
-        }return current.isend
+            }curr = curr.children[char]
+        }return curr.isend
     }
-    suggest(word){
-        let current = this.root
+    autosuggest(word){
+        let curr = this.root
         for(let char of word){
-            if(!current.children[char]){
+            if(!curr.children[char]){
                 return []
-            }current = current.children[char]
+            }curr = curr.children[char]
         }
-        let suggestion = []
+        let autosuggest = []
         let dfs = (node,word) =>{
             if(node.isend){
-                suggestion.push(word)
+                autosuggest.push(word)
             }
             for(let char in node.children){
-                dfs(node.children[char],word+char)
+                dfs(node.children[char], word+char)
             }
         }
-        dfs(current,word)
-        return suggestion
-    }
-    longestprefix(word){
-        let curr = this.root
-        let prefix = ''
-        for(let char of word){
-            if(curr.children[char]){
-                prefix+=char
-                curr = curr.children[char]
-            }else break
-        }return prefix
+        dfs(curr,word)
+        return autosuggest
     }
     commonprefix(){
         let curr = this.root
         let prefix = ''
         while(true){
             let keys = Object.keys(curr.children)
-            if(keys.length == 1 && !curr.isend){
-                prefix+=keys[0]
+            if(keys.length === 1 && !curr.isend){
+                prefix += keys[0]
                 curr = curr.children[keys[0]]
             }else break
         }return prefix
     }
+    longestprefix(word){
+        let curr = this.root
+        let prefix = ''
+        for(let char of word){
+            if(curr.children[char]){
+                prefix += char
+                curr = curr.children[char]
+            }else break
+        }return prefix
+    }
+    delete(word){
+        let curr = this.root
+        let path = []
+        for(let char of word){
+            if(!curr.children[char])return
+            path.push([curr,char])
+            curr = curr.children[char]
+        }
+        if(!curr.isend)return
+        curr.isend = false
+        for(let i=word.length-1;i>=0;i--){
+            let [parent,char] = path[i]
+            let node = parent.children[char]
+            if(Object.keys(node.children).length>0 || node.isend){
+                break
+            }
+            delete parent.children[char]
+            curr = parent
+        }
+    }
 }
-
 let trie = new Trie()
 trie.insert("cat")
-trie.insert("cap")
-trie.insert("cart")
-console.log(trie.search("cat"))
-console.log(trie.suggest("c"))
-console.log(trie.longestprefix("cartle"))
+trie.insert("cammel")
+trie.insert("canamel")
+trie.insert("candle")
+console.log(trie.autosuggest("can"))
 console.log(trie.commonprefix())
+console.log(trie.longestprefix("catterpillar"))
+trie.delete("candle")
+console.log(trie.autosuggest("can"))
+console.log(trie.longestprefix("canamel"))
